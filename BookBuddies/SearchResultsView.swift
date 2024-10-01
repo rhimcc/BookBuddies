1d4d4790eb6a8bc701ad9e50fb75a2751950f53e
rhimcc <rio.mccormack25@gmail.com>
2024-10-01 12:00:15 +1000

    Search updating correctly

--- BookBuddies/SearchResultsView.swift
+++ BookBuddies/SearchResultsView.swift
@@ -9,15 +9,39 @@ import SwiftUI
 
 struct SearchResultsView: View {
     @ObservedObject var viewModel: BookViewModel
-    var body: some View {
-        Text("\(viewModel.searchQuery)")
-        ForEach(viewModel.bookService.books) { book  in
-            Text("\(book.volumeInfo?.title)") // Display each book title
-            }
-        
-    }
     
+    var body: some View {
+        ScrollView {
+            ForEach(viewModel.books) { book in
+                VStack(alignment: .leading) {
+                    // Display the book's thumbnail, if available
+                    if let thumbnail = book.volumeInfo?.imageLinks?.thumbnail {
+                        AsyncImage(url: URL(string: thumbnail)) { phase in
+                            if let image = phase.image {
+                                image
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 120, height: 120)
+                            } else if phase.error != nil {
+                                Text("Error loading image")
+                            } else {
+                                ProgressView() // Show a loading spinner while the image is loading
+                            }
+                        }
+                    } else {
+                        Text("No Image Available")
+                    }
+                    
+                    // Display the book's title, if available
+                    Text(book.volumeInfo?.title ?? "No Title Available")
+                        .font(.headline)
+                }
+                .padding()
+            }
+        }
+    }
 }
+
 
 
 //#Preview {
