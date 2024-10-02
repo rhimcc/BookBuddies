db4831b95565ec7e6391b569756a4ab9eb3188dc
rhimcc <rio.mccormack25@gmail.com>
2024-10-02 17:55:44 +1000

    Changing book model to simplify storing book data

--- BookBuddies/BookModel.swift
+++ BookBuddies/BookModel.swift
@@ -18,7 +18,12 @@ class Book: Decodable, Identifiable{
     let id: String?
     let selfLink: String?
     let volumeInfo: VolumeInfo?
-    let bookshelf: BookshelfModel?
+    let bookshelf: String?
+    let image: String?
+    let readStatus: String?
+    let desc: String?
+    let title: String?
+    let authors: String?
     
     func convertURL() -> String {
         var imageURL: String = volumeInfo?.imageLinks?.thumbnail ?? ""
@@ -39,11 +44,30 @@ class Book: Decodable, Identifiable{
         }
         return ""
     }
-    init(id: String?, selfLink: String?, volumeInfo: VolumeInfo?, bookshelf: BookshelfModel){ // initalising all values for books
+    
+    func getTitle() -> String {
+        if let volumeInfo = self.volumeInfo {
+            return volumeInfo.title
+        }
+        return ""
+    }
+    
+    func getImageThumbnail() -> String {
+        if let volumeInfo = self.volumeInfo {
+            if let imageLinks = volumeInfo.imageLinks {
+                return imageLinks.thumbnail
+            }
+        }
+        return ""
+    }
+    
+    init(id: String?, title: String, authors: String, bookshelf: String, image: String, readStatus: String){ // initalising all values for books
         self.id = id
-        self.selfLink = selfLink
-        self.volumeInfo = volumeInfo
+        self.title = title
+        self.authors = authors
         self.bookshelf = bookshelf
+        self.image = image
+        self.readStatus = readStatus
     }
     
     required init(from decoder: Decoder) throws { //initialises all of the variables for JSON decoding
@@ -51,14 +75,15 @@ class Book: Decodable, Identifiable{
         self.id = try container.decodeIfPresent(String.self, forKey: .id)
         self.selfLink = try container.decodeIfPresent(String.self, forKey: .selfLink)
         self.volumeInfo = try container.decodeIfPresent(VolumeInfo.self, forKey: .volumeInfo)
-            
-
+        self.desc = try container.decodeIfPresent(String.self, forKey: .desc)
+        
         }
     
     enum CodingKeys: String, CodingKey {
         case id
         case selfLink
         case volumeInfo
+        case desc
     }
 }
 
