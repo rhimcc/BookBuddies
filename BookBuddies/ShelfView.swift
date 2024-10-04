1a925b6b68be25791d551eaf9d63a63a8e5e8a5b
rhimcc <rio.mccormack25@gmail.com>
2024-10-04 19:14:59 +1000

    Changing shelf and colours

--- BookBuddies/ShelfView.swift
+++ BookBuddies/ShelfView.swift
@@ -14,7 +14,7 @@ struct ShelfView: View {
     var body: some View {
         ZStack {
             Rectangle()
-                .fill(.darkPeach)
+                .fill(.lightPeach)
                 .frame(width: totalWidth + 20, height: totalHeight + 30, alignment: .center)
                 .offset(y: 15)
             Rectangle() // whole shelf
@@ -25,19 +25,19 @@ struct ShelfView: View {
                 .fill(.darkPeach)
                 .frame(width: totalWidth - 30, height: totalHeight - 20)
                 .offset(y: -10)
-                .shadow(color: .black.opacity(0.2), radius: 10)
+                .shadow(color: .black.opacity(0.2), radius: 2)
             
             Rectangle() // lines for corners
                 .fill(.darkPeach)
                 .frame(width: 2, height: 30)
                 .rotationEffect(Angle(degrees: 36))
-                .offset(x: -totalWidth/2 + 10, y: 50)
+                .offset(x: -totalWidth/2 + 10, y: 45)
                 .shadow(color: .black.opacity(0.2), radius: 10)
             Rectangle()
                 .fill(.darkPeach)
                 .frame(width: 2, height: 30)
                 .rotationEffect(Angle(degrees: 360 - 36))
-                .offset(x: totalWidth/2 - 10, y: 50)
+                .offset(x: totalWidth/2 - 10, y: 45)
                 .shadow(color: .black.opacity(0.2), radius: 10)
             Text(text)
                 .offset(y: 75)
